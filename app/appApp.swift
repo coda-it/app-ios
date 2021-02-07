077a28e571a262f5a24491da06d2fe43088fc417
Oskar Szura <oskar.szura@gmail.com>
2021-02-07 19:46:48 +0100

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Oskar on 07/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
